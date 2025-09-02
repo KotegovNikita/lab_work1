4841b75dfd6da2c5a66cd787f54881d9d0bfc8b8
NikitaKotegov <st128906@student.spbu.ru>
2025-09-02 16:49:53 +0300

    Added libraries for parallelism

--- bmp_reader.hpp
+++ bmp_reader.hpp
@@ -1,52 +1,61 @@
-/* Nikita Kotegov st128906@student.spbu.ru Group B81 Lab_work1 */
-#ifndef BMP_HEADER_HPP
-#define BMP_HEADER_HPP
+/* Nikita Kotegov st128906@student.spbu.ru Group B81 Lab_work1 (Parallel version)*/
+#ifndef BMP_READER_HPP
+#define BMP_READER_HPP
+
 #include <iostream>
-#include <fstream>
-#include <cmath>
 #include <vector>
+#include <cmath>
 #include <algorithm>
+#include <cstdio>
 #include <cstdlib>
 
-#pragma pack(1)
-struct BMPHeader { // Structure that contains file information (identifier, size, unused bytes, offset to pixel data)
-    unsigned char ID[2];
-    unsigned int file_size;
-    unsigned char unused[4];
-    unsigned int pixel_offset;
+#include <thread>
+#include <future>
+#include <mutex>
+#include <atomic>
+#include <numeric>
+
+// -- Структуры для BMP файла --
+
+#pragma pack(push, 1) // Точное выравнивание полей структуры в памяти
+struct BMPHeader {
+    uint16_t file_type{0x4D42};    // "BM"
+    uint32_t file_size{0};         // Размер файла в байтах
+    uint16_t reserved1{0};
+    uint16_t reserved2{0};
+    uint32_t pixel_offset{0};      // Смещение до пиксельных данных
 };
 
-struct DIBHeader { // Structure that contains detailed information about the image
-    unsigned int heder_size; // Header size
-    unsigned int width; // Image dimensions
-    unsigned int height;
-    unsigned short color_planes; // Number of color planes
-    unsigned short bits_per_pixel; // Bits per pixel
-    unsigned int BI_RGB; // Compression type
-    unsigned int data_size; // Size of the image data
-    unsigned int pwidth;
-    unsigned int pheight;
-    unsigned int colors_count;
-    unsigned int imp_colors_count;
+struct DIBHeader {
+    uint32_t header_size{40};      // Размер этой структуры
+    int32_t width{0};              // Ширина изображения в пикселях
+    int32_t height{0};             // Высота изображения в пикселях
+    uint16_t color_planes{1};
+    uint16_t bits_per_pixel{24};   // Глубина цвета (24 бита)
+    uint32_t BI_RGB{0};            // Без сжатия
+    uint32_t data_size{0};         // Размер пиксельных данных
+    int32_t h_resolution{0};
+    int32_t v_resolution{0};
+    uint32_t colors_in_palette{0};
+    uint32_t important_colors{0};
 };
+#pragma pack(pop)
 
 struct BMPFile {
     BMPHeader bhdr;
     DIBHeader dhdr;
-    unsigned char* data;
+    unsigned char* data{nullptr};
 };
 
+// -- Объявления функций --
 
-void save_BMPFile(const BMPFile* bmp_file, const char* file_name);   // Saves the BMPFile structure to a file
-size_t calculateBMPDataSize(const DIBHeader& dibHeader);             // Calculates the size of the image data, considering alignment
-BMPFile* loadBMPFile(char* fname);                                   // Loads a BMP file from disk
-void freeBMPFile(BMPFile* bmp_file);                                 // Frees the allocated memory for pixel data and the BMPFile structure
+void save_BMPFile(const BMPFile* bmp_file, const char* file_name);
+size_t calculateBMPDataSize(const DIBHeader& dibHeader);
+BMPFile* loadBMPFile(const char* fname);
+void freeBMPFile(BMPFile* bmp_file);
+BMPFile* rotateBMP90Clockwise(const BMPFile* original);
+BMPFile* rotateBMP90Counterclockwise(const BMPFile* original);
+std::vector<float> createGauss1D(int size, float sigma);
+void applyGaussSeparable(BMPFile* bmp, int kernelSize, float sigma);
 
-BMPFile* rotateBMP90Clockwise(const BMPFile* original);              // Rotates the image 90 degrees clockwise
-BMPFile* rotateBMP90Counterclockwise(const BMPFile* original);       // Rotates the image 90 degrees counterclockwise
-
-std::vector<float> createGauss1D(int size, float sigma);             // Creates a 1D Gaussian kernel
-void applyGaussSeparable(BMPFile* bmp, int kernelSize, float sigma); // Applies Gaussian blur using separable convolution
-
-
-#endif
+#endif //BMP_READER_HPP
